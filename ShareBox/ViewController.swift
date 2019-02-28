410dc94c2c1fb1dc84785c4a6b52a8fb1feedd46
Сергей Иванов <sergejivanov@Mac-mini-Sergej.local>
2019-02-28 15:27:51 +0300

    share

--- ShareBox/ViewController.swift
+++ ShareBox/ViewController.swift
@@ -10,11 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func share(sender: UIButton) {
+        guard let image = imageView.image else { return }
+        
+        let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        activityController.popoverPresentationController?.sourceView = sender
+        
+        present(activityController, animated: true, completion: nil)
+    }
+    
+    func safari(sender: UIButton) {
+        
+    }
+    
+    func camera(sender: UIButton) {
+        
+    }
+    
+    func email(sender: UIButton) {
+        
+    }
+    
+    @IBAction func buttonPressed(sender button: UIButton) {
+        guard let text = button.titleLabel?.text else { return }
+        
+        switch text {
+        case "Share":
+            share(sender: button)
+        case "Safari":
+            safari(sender: button)
+        case "Camera":
+            camera(sender: button)
+        case "E-mail":
+            email(sender: button)
+        default:
+            break
+        }
+        
+    }
 
 }
 
